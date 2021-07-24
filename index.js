00ed004276931f3581a11fa1af5d23383b5078a9
Sari Pagurek van Mossel <sari.pvm@gmail.com>
2021-07-24 15:07:42 -0400

    File upload

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const express = require('express')
+const express = require('express');
 const exphbs = require('express-handlebars');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+const publicFolder = './public/images';
+const fs = require('fs');
+const busboy = require('connect-busboy');
+const uuid = require('uuid');
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+app.use(busboy());
 
 app.get('/', (req, res) => {
-  res.render('home');
-})
+  const filenames = fs.readdirSync(publicFolder);
+  res.render('home', {
+    filenames: filenames,
+  });
+});
+
+app.post('/upload', (req, res) =>{
+  req.pipe(req.busboy);
+  req.busboy.on('file', (fieldname, file, filename) => {
+    const fstream = fs.createWriteStream(publicFolder + '/' + uuid.v4() + filename);
+    file.pipe(fstream);
+    fstream.on('close', () => {
+      res.redirect('/');
+    });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
