0d81886ffade78eac43128f4dd2deaf4a75c84a3
Sari Pagurek van Mossel <sari.pvm@gmail.com>
2021-07-24 14:24:01 -0400

    Send back HTML

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const exphbs = require('express-handlebars');
 const app = express()
 const port = 3000
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-  res.send('sari was here')
+  res.render('home');
 })
 
 app.listen(port, () => {
